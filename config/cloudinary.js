8a7c190ca2edf27f5764410100c4bbdc8200e2ea
ZdapX <stokmlmurmer150@gmail.com>
2025-12-31 14:04:17 +0700

    Create cloudinary.js

---
+++ config/cloudinary.js
@@ -0,0 +1,7 @@
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: 'dnb0q2s2h',
+    api_key: '838368993294916',
+    api_secret: 'N9U1eFJGKjJ-A8Eo4BTtSCl720c'
+});
+module.exports = cloudinary;
