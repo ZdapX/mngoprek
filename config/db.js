b3088337f082c9715f36227da2b0df5b475d4507
ZdapX <stokmlmurmer150@gmail.com>
2025-12-31 14:24:31 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,20 @@
+
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
+    if (mongoose.connection.readyState >= 1) return;
+
     try {
-        await mongoose.connect('mongodb+srv://dafanation1313_db_user:hAMuQVA4A1QjeUWo@cluster0.d28log3.mongodb.net/?appName=Cluster0');
+        await mongoose.connect('mongodb+srv://dafanation1313_db_user:hAMuQVA4A1QjeUWo@cluster0.d28log3.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 10000, // 10 detik timeout
+        });
         console.log('Cyberlink Established: MongoDB Connected');
     } catch (err) {
-        console.error(err);
+        console.error('Database Connection Error:', err.message);
+        // Jangan biarkan aplikasi crash, tapi log errornya
     }
 };
+
 module.exports = connectDB;
